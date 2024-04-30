1d8f2979aac7a3609bd00ea32dbd42e4fbdc9fbc
Ronald Espitia <ronald79@gmail.com>
2024-04-29 22:39:53 -0500

    Clase Abril 25

---
+++ src/Abril25.ts
@@ -0,0 +1,32 @@
+// const tipo: string = 'Secundario';
+// const color: string = 'Azul';
+
+// if (tipo === 'Primario') {
+//     if (color === 'Azul') {
+//         console.log(color + ' es un color PRIMARIO');
+//     } else {
+//         console.log(color + ' NOO es un color PRIMARIO');
+//     }
+// } else {
+//     if (color === 'Verde') {
+//         console.log(color + ' es un color SECUNDARIO');
+//     } else {
+//         console.log(color + ' NOO es un color SECUNDARIO');
+//     }
+// }
+const opcion: string = 'J';
+
+switch (opcion) {
+    case 'R':
+        console.log('La opcion es R');
+        break;
+    case 'Z':
+        console.log('La opcion es Z');
+        break;
+    default:
+        console.log('NO RECONOZCO');
+}
+
+console.log('DESPUES DEL SWITCH');
+
+
