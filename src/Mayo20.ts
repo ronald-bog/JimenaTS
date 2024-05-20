4c8a5d1896c47a3a7f68f0fa21fa7d7a36a6d5d0
Ronald Espitia <ronald79@gmail.com>
2024-05-20 17:09:12 -0500

    Clase Mayo 20

---
+++ src/Mayo20.ts
@@ -0,0 +1,25 @@
+
+let tupla: readonly [number, string, boolean] = [5, 'hola', true];
+
+// tupla.push(5); // error de TS
+
+console.log(tupla);
+
+//tupla[0] = 11;
+
+// console.log(tupla);
+
+// destructuracion de tuplas
+
+let [pepito, fulanito, jimena] = tupla;
+
+console.log(pepito);
+console.log(fulanito);
+console.log(jimena);
+
+// Rest parameters
+let tupla2: [number, ...string[]] = [556, 'Jimena', 'Ronald', 'Luis'];
+
+console.log(tupla2[1]);
+
+// FUNCIONES codigo en la proxima clase
