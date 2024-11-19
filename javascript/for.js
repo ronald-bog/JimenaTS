47d517f919ec9f893064c515f3804438665ea7e3
Ronald Espitia <ronald79@gmail.com>
2024-11-18 20:28:47 -0500

    Clase

---
+++ javascript/for.js
@@ -0,0 +1,25 @@
+// FOR
+
+/* for (let i = 0; i < 5; i++) {
+    console.log(i);
+} */
+
+// let a = 10;
+// a++;
+// console.log(a);
+
+// github.com
+
+let suma = 0;
+for (let valor = 1; valor <= 5; valor++) {
+    //suma = suma + valor;
+    suma += valor;
+}
+
+console.log(suma);
+
+const nombres = ['Jimena', 'Sofia', 'Karolyn', 'Lina', 'Nicol'];
+
+for (let indice = 0; indice < nombres.length; indice++) {
+    console.log(nombres[indice]);
+}
---
+++ javascript/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+let a = 10;
+
+a--;
+console.log(a);
